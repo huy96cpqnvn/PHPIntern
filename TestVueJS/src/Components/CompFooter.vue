cceab820cf853f2d1280038fd2c17049408bdf60
huy96cpqnvn <huy96cpqnvn@gmail.com>
2020-08-05 15:04:22 +0700

    Test VueJS

---
+++ TestVueJS/src/Components/CompFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    <h1>
+      {{ textFooter }}
+    </h1></footer>
+</template>
+<script>
+export default  {
+  name: 'comp-footer',
+  data() {
+    return {
+      textFooter: 'Hello Footer'
+    }
+  }
+}
+</script>
+<style></style>
---
+++ TestVueJS/src/Components/CompHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header><h1>{{ text }}</h1></header>
+</template>
+
+<script>
+  export default {
+    name: 'comp-header',
+    data() {
+      return {
+        text: 'Hello World'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
